accbd898e7fbbb45a3802f9d4ab13d12406c3719
Md Bappy <mdbappy1350@gmail.com>
2023-08-30 22:26:39 +0600

    marge sort

--- sort/margeSort.c
+++ sort/margeSort.c
@@ -1,107 +1,80 @@
 //complxity best case = age case= worst case = n log(n)
 #include <stdio.h>
-#include <stdlib.h>
 
 void print(int arr[], int n);
-
 void divide(int arr[], int L, int R);
-
-void conqure(int arr[], int L, int mid, int R);
+void marge(int arr[], int L, int mid, int R);
 
 void main() {
-    //taking array size.
-    int n;
+    int n;  //array size.
     printf("Enter how many element : ");
-    scanf("%d", &n);
-    int arr[n];
+    scanf("%d", &n);//input array size
 
-    //taking array element as user input.
+    int arr[n];//declare arrray
     printf("Enter the Element : ");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)  //taking array element from user
         scanf("%d", &arr[i]);
 
-    //printing unsorted array.
     printf("Before sorted the array : ");
     print(arr, n);
 
-    //call method for sort.
-    divide(arr, 0, n - 1);
-
-    //printing sorted array.
+    divide(arr, 0, n - 1);//call method for sort.
     printf("After sorted the array : ");
     print(arr, n);
 }
-
-
+//print array by function
 void print(int arr[], int n) {
-    //printing array using for loop.
     for (int i = 0; i < n; i++)
         printf("%d\t", arr[i]);
     printf("\n");
 }
 
-
 void divide(int arr[], int L, int R) {
-    //declear mid variable .
-    int mid = 0;
+    int mid = 0;//declear mid variable .
+    if (L < R) {  // condition true when Left is smaller than Right.
+        mid = (L + R) / 2;//calulate mid value .
+        divide(arr, L, mid);  //use recurtion for divide array's Left part in single element.
 
-    // condition true when Left is smaller than Right.
-    if (L < R) {
-        //calulate mid value .
-        mid = (L + R) / 2;
-
-        //use recurtion for divide array's Left part in single element.
-        divide(arr, L, mid);
-        //again use recurtion for divide array's right part in single element.
-        divide(arr, mid + 1, R);
-
-        //conqure the single element for build the array.
-
-        conqure(arr, L, mid, R);
+        divide(arr, mid + 1, R);  //again use recurtion for divide array's right part in single element.
+        marge(arr, L, mid, R); //marge the single element for build the array.
     }
 }
 
-
-void conqure(int arr[], int L, int mid, int R) {
-    int i, j, k=L;
+//marge the divided array and sort.
+void marge(int arr[], int L, int mid, int R) {
+    int i, j;
     //create two variable that indicate new array size.
     int n1 = mid - L + 1;
     int n2 = R - mid;
 
-    //declared two array.
-    int arrL[n1], arrR[n2];
-    //copy data to the array.
-    for (i = 0; i < n1; i++)
-        arrL[i] = arr[L + i];
+    int arrL[n1], arrR[n2]; //declared two array.
 
+    for (i = 0; i < n1; i++) //copy data to the array.
+        arrL[i] = arr[L + i];
     for (j = 0; j < n2; j++)
         arrR[j] = arr[mid + 1 + j];
 
     i = 0;
     j = 0;
-    //sort the short array.
-    while (i < n1 && j < n2) {
+    while (i < n1 && j < n2) {  //sorting array
         if (arrL[i] <= arrR[j]) {
-            arr[k] = arrL[i];
+            arr[L] = arrL[i];
             i++;
         } else {
-            arr[k] = arrR[j];
+            arr[L] = arrR[j];
             j++;
         }
-        k++;
+        L++;
     }
 
-    //copy remaining element of arrL
-    while (i < n1) {
-        arr[k] = arrL[i];
+    while (i < n1) {     //copy remaining element of arrL
+        arr[L] = arrL[i];
         i++;
-        k++;
+        L++;
     }
-
-    //copy remaining element of arrR.
-    while (j < n2) {
-        arr[k] = arrR[j];
+    while (j < n2) {      //copy remaining element of arrR.
+        arr[L] = arrR[j];
         j++;
-        k++;
+        L++;
     }
 }
