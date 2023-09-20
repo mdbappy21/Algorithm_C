2ba44fcbe3e6d32453f69c5f6980b371fd70852d
Md Bappy <mdbappy1350@gmail.com>
2023-09-20 17:50:58 +0600

    fix problem

--- Greedy Algorithm/Fractional_knapsack.c
+++ Greedy Algorithm/Fractional_knapsack.c
@@ -1,61 +1,65 @@
 #include <stdio.h>
 
-struct knapsack{
+typedef struct knapsack {
     float weight;
     float profit;
-    float dencity;
-};
+    float density;
+}knapsack;
 
-int main(){
+int main() {
     int n;
     float Sack_Capacity;
 
-    float Max_weight=0,Max_profit=0;
+    float Max_weight = 0, Max_profit = 0;
 
-    printf("Enter how much weight : ");
-    scanf("%d",&n);
-    struct knapsack p[n];
+    printf("Enter how many items: ");
+    scanf("%d", &n);
+    knapsack p[n];
     float x[n];
-    for(int i=0;i<n;i++){
-        printf("Enter Weight and Profit : ");
-        scanf("%f",&p[i].weight);
-        scanf("%f",&p[i].profit);
-        p[i].dencity=(p[i].profit)/(p[i].weight);
-        printf("%d\t%d\t%d\n",p[i].weight,p[i].profit,p[i].dencity);
+    for (int i = 0; i < n; i++) {
+        printf("Enter Weight and Profit for item %d: ", i + 1);
+        scanf("%f", &p[i].weight);
+        scanf("%f", &p[i].profit);
+        p[i].density = (p[i].profit) / (p[i].weight);
     }
 
-    for(int i=0;i<n-1;i++)
-        for(int j=0;j<n;j++){
-            if(p[i].dencity<p[j].dencity){
-                struct knapsack temp=p[i];
-                p[i]=p[j];
-                p[j]=temp;
+    // Sorting items based on density in descending order
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (p[i].density < p[j].density) {
+                knapsack temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
             }
         }
+    }
 
+    printf("Enter Sack capacity: ");
+    scanf("%f", &Sack_Capacity);
 
-    printf("Enter Sack capacity : ");
-    scanf("%d",&Sack_Capacity);
-
-
-    while(Sack_Capacity!=0){
-        int i=0;
-        if(p[i].weight<Sack_Capacity){
-            x[i]=1;
-            Sack_Capacity-=p[i].weight;
-        }
-        else{
-            x[i]=Sack_Capacity/p[i].weight;
-            Sack_Capacity-=p[i].weight*x[i];
+    int i = 0;
+    while (Sack_Capacity > 0 && i < n) { // Changed the condition from Sack_Capacity != 0 to Sack_Capacity > 0
+        if (p[i].weight <= Sack_Capacity) {
+            x[i] = 1;
+            Sack_Capacity -= p[i].weight;
+        } else {
+            x[i] = Sack_Capacity / p[i].weight;
+            Sack_Capacity = 0; // Set Sack_Capacity to 0 when we cannot fit more items
         }
         i++;
     }
 
-    for(int i=0;i<n;i++){
-        Max_weight+=x[i]*p[i].weight;
-        Max_profit+=x[i]*p[i].profit;
+    for (int j = 0; j < n; j++) {
+        Max_weight += x[j] * p[j].weight;
+        Max_profit += x[j] * p[j].profit;
     }
-    printf("Max Weight : %f\n",Max_weight);
-    printf("Max profit : %f\n",Max_profit);
+    printf("Max Weight: %.2f\n", Max_weight);
+    printf("Max Profit: %.2f\n", Max_profit);
 
+    if (Sack_Capacity > 0)
+        printf("Sack Capcity remain : %.2f\n", Sack_Capacity);
+    else
+        printf("Sack is full\n");
+
+    return 0;
 }
