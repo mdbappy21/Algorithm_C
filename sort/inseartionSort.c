74de06cadb0ba04982c84d9d5221d2cdce2732a1
Md Bappy <mdbappy1350@gmail.com>
2023-10-05 07:21:48 +0600

    Inseartion Sort

--- sort/inseartionSort.c
+++ sort/inseartionSort.c
@@ -34,9 +34,15 @@ void print(int arr[], int n)
 //sort the array
 void insertionSort(int arr[], int n)
 {
-    for (int i = 0; i < n; i++)
-        while (i >= 0 && arr[i] > arr[i+1]) {
-            arr[i] = arr[i+1];
-            arr[i + 1] = arr[i];
+    int i, temp, j;
+    for (i = 1; i < n; i++) {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j+1]=arr[j+1]+arr[j];    //swip arr[j+1] and arr[j]
+            arr[j] = arr[j+1]-arr[j];
+            arr[j+1] = arr[j+1]-arr[j];
+            j --;
         }
+    }
 }
