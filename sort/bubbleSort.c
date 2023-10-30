ce6a79c27e38713202fc8b7246d81e27fdfabed8
Md Bappy <mdbappy1350@gmail.com>
2023-10-30 10:57:33 +0600

    bubble sort

--- sort/bubbleSort.c
+++ sort/bubbleSort.c
@@ -31,7 +31,7 @@ void print(int arr[], int n) { //print an array
 void bubbleSort(int arr[], int n) {
     int temp;
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < n -i- 1; j++)
             if (arr[j] > arr[j + 1]) {
                 swap(&arr[j], &arr[j + 1]);
                 temp = 1;
